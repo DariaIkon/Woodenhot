2197cd6df1a8be7fb11b84d2203dbb6bc6098f9f
devisd <89788799+devisd@users.noreply.github.com>
2022-05-08 04:40:40 +0700

    add two sliders

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,9 +1,4 @@
- // import Swiper JS
-import Swiper from 'swiper';
-  // import Swiper styles
-import '../styles/swiper.css';
-
-var swiper = new Swiper(".mySwiper", {
+const firstSlider = new Swiper(".image-swiper", {
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
@@ -11,17 +6,40 @@ var swiper = new Swiper(".mySwiper", {
         // Динамические буллеты
         dynamicBullets:true,
         // Автопрокрутка
-        // autoplay: {
-        //     // Пауза между прокруткой
-        //     delay: 3500,
-        //     // Закончить на последнем слайде
-        //     stopOnLastSlide: false,
-        //     // Отключение после ручного переключения
-        //     disableOnInteraction: false,
-        // },
+        autoplay: {
+            // Пауза между прокруткой
+            delay: 4000,
+            // Закончить на последнем слайде
+            stopOnLastSlide: false,
+            // Отключение после ручного переключения
+            disableOnInteraction: false,
+        },
         // Скорость прокрутки
         speed: 800,
         // Бесконечный слайдер
-        loop: true
-    });
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 10,
+});
 
+const secondSlider = new Swiper(".textSlider", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        // Динамические буллеты
+        dynamicBullets:true,
+        // Автопрокрутка
+        autoplay: {
+            // Пауза между прокруткой
+            delay: 2200,
+            // Закончить на последнем слайде
+            stopOnLastSlide: false,
+            // Отключение после ручного переключения
+            disableOnInteraction: false,
+        },
+        // Скорость прокрутки
+        speed: 800,
+        // Бесконечный слайдер
+        loop: true,
+});
