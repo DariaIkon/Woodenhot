82816c43bcbf2a098bbb7bb59785b10937535c75
Alfat1989 <gilmalf89@gmail.com>
2022-05-07 11:07:14 +0300

    add swiper slider

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,1 @@
-//  // import Swiper JS
-//   import Swiper from 'swiper';
-//   // import Swiper styles
-//   import 'swiper/css';
-
-//   const swiper = new Swiper(...);
+import swiper from './swiper'
---
+++ src/scripts/swiper.js
@@ -0,0 +1,27 @@
+ // import Swiper JS
+import Swiper from 'swiper';
+  // import Swiper styles
+import '../styles/swiper.css';
+
+var swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        // Динамические буллеты
+        dynamicBullets:true,
+        // Автопрокрутка
+        // autoplay: {
+        //     // Пауза между прокруткой
+        //     delay: 3500,
+        //     // Закончить на последнем слайде
+        //     stopOnLastSlide: false,
+        //     // Отключение после ручного переключения
+        //     disableOnInteraction: false,
+        // },
+        // Скорость прокрутки
+        speed: 800,
+        // Бесконечный слайдер
+        loop: true
+    });
+
